2c702259815158a73b689f88dff3fe78562bd58d
AAMY <abdelrahmansketchware@gmail.com>
2024-01-10 01:14:39 +0200

    MY COMMIT

--- Dice Roller.cpp
+++ Dice Roller.cpp
@@ -1,41 +1,13 @@
 #include<iostream>
 #include<cstdlib>
+#include "ABODAstd.h"
 
 using namespace std;
-
-short RandomNumber(short From, short To)
-{
-	short RandomNumber = rand() % (To - From + 1) + From;
-
-	return RandomNumber;
-}
 
 void DiceRoller()
 {
 	cout << "DICE 1 FACE = " << RandomNumber(1, 6) << "\n";
 	cout << "DICE 2 FACE = " << RandomNumber(1, 6) << "\n";
-}
-
-bool DoYouWantToPlayAgain()
-{
-	string Choice;
-	do
-	{
-		cout << "DO YOU WANT TO PLAY AGAIN[Y/N]\n";
-		cin >> Choice;
-	} while (Choice != "Y" && Choice != "y" && Choice != "N" && Choice != "n");
-
-	if (Choice == "Y" || Choice == "y")
-	{
-		system("cls");
-		return 1;
-	}
-
-	else
-	{
-		cout << "GOOD BYE :)\n";
-		return 0;
-	}
 }
 
 void StartDiceRoller()
